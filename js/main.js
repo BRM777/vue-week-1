c5be1e0acbf911514173c7465a7381678be24ebc
Ruslan Beschetnikoff <beschetnikof@yandex.ru>
2023-03-17 22:13:02 +0700

    practical work 4

--- js/main.js
+++ js/main.js
@@ -2,12 +2,22 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./img/vmSocks-blue-onWhite.jpg",
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
         inStock: true,
         inventory: 100,
         onSale:true
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
