33ff7d59129e8f91cd01fa49eee0303838c76144
Ruslan Beschetnikoff <beschetnikof@yandex.ru>
2023-03-17 22:26:09 +0700

    practical work 5

--- js/main.js
+++ js/main.js
@@ -5,19 +5,36 @@ let app = new Vue({
         image: "./img/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100,
-        onSale:true
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./img/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
                 variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./img/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        subtractionToCart(){
+            if(this.cart){
+                this.cart -=1;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
 })
+0 comments on commit de32e43
